e12acc38b31d5587f4c8e4a7a56aadf3f23421eb
guiltygear12 <mylove700757@gmail.com>
2022-04-05 20:53:19 +0900

    4월 5일 메인 슬라이드 컨텐츠 제작 70% 완료

---
+++ JS/main.js
@@ -0,0 +1,51 @@
+let slideNum=0;
+function slideCount(slideNum) {
+    return slideNum + 1;
+  }
+// 슬라이드 배너
+// 넥스트 버튼
+$('.slide_next').mouseenter(function(){
+    switch (slideNum) {
+        case 0:
+            $('.slide_item:nth-child(2)').css({'transform':'scale(1.3)'})
+          break;
+        case 1:
+            $('.slide_item:nth-child(3)').css({'transform':'scale(1.3)'})
+          break;
+
+      }
+})
+$('.slide_next').mouseleave(function(){
+    switch (slideNum) {
+        case 0:
+            $('.slide_item:nth-child(2)').css({'transform':'scale(1)'})
+          break;
+        case 1:
+            $('.slide_item:nth-child(3)').css({'transform':'scale(1)'})
+          break;
+
+      }
+})
+$('.slide_next').click(function(){
+            
+    switch (slideNum) {
+        case 0:
+            slideNum=1
+            $('.slide_item:nth-child(2)').css({'transform':'scale(1)'})
+            $('.slide_content').css({'transform':'translateX(-100vw)'})
+            break;
+        case 1:
+            slideNum=2
+            $('.slide_item:nth-child(3)').css({'transform':'scale(1)'})
+            $('.slide_content').css({'transform':'translateX(-200vw)'})
+            break;
+        case 2:
+            slideNum=0
+            $('.slide_item:nth-child(1)').css({'transform':'scale(1)'})
+            $('.slide_content').css({'transform':'translateX(0)'})
+            break;
+        default:
+            alert(slideNum)
+      }
+})
+// 이전 버튼
