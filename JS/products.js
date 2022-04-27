3a39827b7712ad16b03a4751b0dd89a27215f3b0
guiltygear12 <mylove700757@gmail.com>
2022-04-27 16:46:20 +0900

    1

--- JS/products.js
+++ JS/products.js
@@ -1,3 +1,8 @@
 $(".filter_btn").click(function(){
     $(".products_filter").toggleClass("active");
 })
+
+$(".wish_btn").click(function(){
+    $(".fa-heart").toggleClass("active");
+    $(".fa-heart-o").toggleClass("active");
+})
